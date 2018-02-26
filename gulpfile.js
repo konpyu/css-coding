e0fdadbade698504627cc3a878397c68cb5e6b70
Tatsuya Sawanobori <info@swn.jp>
2018-02-27 01:29:41 +0900

    iconfont 生成

--- gulpfile.js
+++ gulpfile.js
@@ -9,49 +9,36 @@ const reload = browser.reload;
 const iconfont = require('gulp-iconfont');
 const consolidate = require('gulp-consolidate');
 const rename = require('gulp-rename');
-const runTimestamp = Math.round(Date.now() / 1000);
 
 const SRC = './src/';
 const PUBLIC = './public/';
 
-gulp.task('font', function() {
-  const fontName = 'icon';
-
+const runTimestamp = Math.round(Date.now() / 1000);
+gulp.task('iconfonts', function() {
   return gulp.src(SRC + 'iconfont/*.svg')
     .pipe(iconfont({
-      fontName: fontName,
-      timestamp: runTimestamp,
-      fontHeight: 1001
+      startUnicode: 0xF001,
+      fontName: 'icon',
+      formats: ['ttf', 'eot', 'woff', 'svg'],
+      appendCodepoints: false,
+      normalize: true,
+      fontHeight: 500,
+      timestamp: runTimestamp
     }))
-    .on('codepoints', function(codepoints) {
-
-      //順序制御用の番号を削除
-      for (const i = 0; i < codepoints.length; i++) {
-        codepoints[i]["name"] = codepoints[i]["name"].split('_')[1];
-      }
-      const options = {
-        className: fontName,
-        fontName: fontName,
-        fontPath: '../fonts/',
-        glyphs: codepoints
-      };
-
-      // CSS
-      gulp.src(SRC + 'iconfont/template.css')
-        .pipe(consolidate('lodash', options))
-        .pipe(rename({
-          basename: '_iconfont',
-          extname: '.scss'
+    .on('glyphs', function(glyphs) {
+      gulp.src(SRC + 'iconfont/_iconfont.scss')
+        .pipe(consolidate('lodash', {
+          glyphs: glyphs.map(function(glyph) {
+            return {
+              fileName: glyph.name,
+              codePoint: glyph.unicode[0].charCodeAt(0).toString(16).toUpperCase()
+            };
+          }),
+          fontName: 'icon',
+          fontPath: '../fonts/',
+          cssClass: 'icon'
         }))
         .pipe(gulp.dest(SRC + 'scss/utility/'));
-
-      // フォント一覧 HTML
-      gulp.src(SRC + 'iconfont/template.html')
-        .pipe(consolidate('lodash', options))
-        .pipe(rename({
-          basename: 'icon-sample'
-        }))
-        .pipe(gulp.dest(SRC + 'iconfont/'));
     })
     .pipe(gulp.dest(PUBLIC + 'assets/fonts'));
 });
@@ -91,13 +78,12 @@ gulp.task('server', () => {
     },
     notify: false,
     open: 'external',
-    port: 8080,
+    port: 34019,
     ghostMode: false
   });
 });
 
-gulp.task('default', ['ejs', 'server', 'sass', 'font'], () => {
+gulp.task('default', ['ejs', 'server', 'sass'], () => {
   gulp.watch([SRC + 'ejs/**/*.ejs'], ['ejs']);
   gulp.watch([SRC + 'scss/**/*.scss'], ['sass']);
-  gulp.watch([SRC + 'iconfont/*'], ["font"]);
 });
